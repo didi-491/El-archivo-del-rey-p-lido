371a4c917bb16bfa0e28c279190b2a47a199148b
Diana Campos <dcamposlopez@uoc.edu>
2025-11-26 11:55:38 +0000

    26/11

---
+++ models/staging/stg_hk_raw__hk_charms.sql
@@ -0,0 +1,11 @@
+{{
+  config(
+    materialized='view',
+    comment='A view with the differents areas of the map and their coordenates.'
+  )
+}}
+
+SELECT
+    *
+FROM
+  {{ source('hk_raw', 'hk_charms') }}
